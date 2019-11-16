8ecc41eed768165f8b6dd8216859dea423e92edc
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-11-15 22:52:24 -0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# <%= f.fields_for :locations do |addr| %>
-#     <%= addr.label :city %>
-#     <%= addr.text_field :city, value:@user.items.last.location.city if @user.items  %><br>
- 
-#     <%= addr.label :state %>
-#     <%= addr.text_field :state, value:@user.items.last.location.state if @user.items  %><br>
 
-#     <%= addr.label :zipcode %>
-#     <%= addr.number_field :zipcode, value:@user.items.last.location.zipcode if @user.items  %><br>
-#     <% end %>
+Location.create(city: "Redmond", state: "WA", zipcode: 98053)
+Location.create(city: "bellevue", state: "WA", zipcode: 98008)
+Location.create(city: "Redmond", state: "WA", zipcode: 98053)
+Location.create(city: "Seattle", state: "WA", zipcode: 98002)
+Location.create(city: "Seattle", state: "WA", zipcode: 98004)
+Location.create(city: "Kirkland", state: "WA", zipcode: 98122)
+Location.create(city: "Renton", state: "WA", zipcode: 98202)
